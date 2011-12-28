cee6693040ce460948ea380fd5677bc44cc403b3
Andreas M. Antonopoulos <andreas@antonopoulos.com>
2011-12-27 23:15:36 -0800

    identity factories: IP, MAC, Device, Access Request, Identity

--- ifmap.py
+++ ifmap.py
@@ -1,3 +1,11 @@
+#!/usr/bin/python
+#
+# Copyright 2011, Infoblox, All Rights Reserved
+#
+# Open Source, see LICENSE
+# 
+
+
 from pysimplesoap.simplexml import SimpleXMLElement
 from pysimplesoap.client import SoapClient
 import urllib2
@@ -32,7 +40,7 @@ publish_blob = """<ifmap:publish session-id=\"%(sessionid)s\"><update lifetime="
 </ifmap:publish>"""
 
 class ifmapClient:
-    
+    """ IF-MAP Client """
     server = None
     session = None
     soapclient = None
@@ -63,14 +71,12 @@ class ifmapClient:
             self.session = {'sessionid': response.newSessionResult['session-id'],
                 'publisher_id': response.newSessionResult['ifmap-publisher-id'],
             }
-            
-            
+                  
     def call(self, method, body):
         request = envelope % {'body':body}
         response = self.soapclient.send(method, request)
         responseXML = SimpleXMLElement(response, namespace=namespaces['ifmap'])
         return responseXML
-    
     
     def publishtest(self):
         username = 'jdoe'
@@ -79,4 +85,153 @@ class ifmapClient:
         response = self.call('publish', publish_req)
         return response
         
-    
+class ifmapIDFactory:
+	pass
+
+class IPAddress(ifmapIDFactory):
+	"""
+	XML Factory for an IP Address IF-MAP Identifier
+	"""
+	
+	def __init__(self, ip_address, type=None, administrative_domain=None):
+		self.__ip_address = ip_address
+		self.__type = type
+		self.__administrative_domain = administrative_domain
+		return None;
+		
+	def administrative_domain(self):
+		return self.__administrative_domain
+	
+	def ip_address(self):
+		return self.__ip_address
+	
+	def type(self):
+		return self.__type
+	
+	def __str__(self):
+		self.__XML = "<ip-address"
+		self.__XML += ' value="'+self.__ip_address+'"'
+		# type and administrative_domain are optional 
+		if self.__type:
+			self.__XML +=' type="'+self.__type+'"'
+		if self.__administrative_domain:
+			self.__XML += ' administrative-domain="'+self.__administrative_domain+'"'
+		self.__XML += " />"
+		return self.__XML
+	
+class MACAddress(ifmapIDFactory):
+	"""
+	XML Factory for a MAC Address IF-MAP Identifier
+	"""
+	
+	def __init__(self, mac_address, administrative_domain=None):
+		self.__mac_address = mac_address
+		self.__administrative_domain = administrative_domain
+		return None;
+		
+	def administrative_domain(self):
+		return self.__administrative_domain
+	
+	def mac_address(self):
+		return self.__mac_address
+	
+	def __str__(self):
+		self.__XML = "<mac-address"
+		self.__XML += ' value="'+self.__mac_address+'"'
+		# administrative_domain is optional 
+		if self.__administrative_domain:
+			self.__XML += ' administrative-domain="'+self.__administrative_domain+'"'
+		self.__XML += " />"
+		return self.__XML
+	
+class Device(ifmapIDFactory):
+	"""
+	XML Factory for a Device IF-MAP Identifier
+	"""
+	
+	def __init__(self, name, aik_name=None):
+		self.__name = name
+		self.__aik_name = aik_name
+		return None;
+		
+	def aik_name(self):
+		return self.__aik_name
+	
+	def name(self):
+		return self.__name
+	
+	def __str__(self):
+		self.__XML = "<device>"
+		self.__XML += '<name>'+self.__name+'</name>'
+		# aik_name is optional 
+		if self.__aik_name:
+			self.__XML += '<aik-name>'+self.__aik_name+'<aik-name>'
+		self.__XML += "</device>"
+		return self.__XML
+	
+class AccessRequest(ifmapIDFactory):
+	"""
+	XML Factory for an Access Request IF-MAP Identifier
+	"""
+	
+	def __init__(self, name, administrative_domain=None):
+		self.__name = name
+		self.__administrative_domain = administrative_domain
+		return None;
+		
+	def administrative_domain(self):
+		return self.__administrative_domain
+	
+	def name(self):
+		return self.__name
+	
+	def __str__(self):
+		self.__XML = "<access-request"
+		self.__XML += ' name="'+self.__name+'"'
+		# administrative_domain is optional 
+		if self.__administrative_domain:
+			self.__XML += ' administrative-domain="'+self.__administrative_domain+'"'
+		self.__XML += " />"
+		return self.__XML
+
+class Identity(ifmapIDFactory):
+	"""
+	XML Factory for an IF-MAP Identifier
+	"""
+	
+	def __init__(self, name, type=None, other_type=None, administrative_domain=None):
+		self.__name = name # required
+		self.__type = type # "aik_name"|"distinguished_name"|"dns_name"|"email_address"|"kerberos_principal"|"username"|"sip_uri"|"tel_uri"|"hip_hit"|"other"
+		self.__other_type = other_type # vendor-specific type 
+		self.__administrative_domain = administrative_domain
+		return None;
+		
+	def administrative_domain(self):
+		return self.__administrative_domain
+	
+	def name(self):
+		return self.__name
+	
+	def type(self):
+		return self.__type
+	
+	def other_type(self):
+		return self.__other_type
+	
+	def __str__(self):
+		self.__XML = "<identity"
+		self.__XML += ' name="'+self.__name+'"'
+		# type and administrative_domain are optional 
+		if self.__type:
+			self.__XML +=' type="'+self.__type+'"'
+		if self.__other_type:
+			self.__XML +=' other-type="'+self.__other_type+'"'
+		if self.__administrative_domain:
+			self.__XML += ' administrative-domain="'+self.__administrative_domain+'"'
+		self.__XML += " />"
+		return self.__XML
+	
+if __name__ == "__main__":
+	print """The ifmap client library is not meant to be run from the command line or python interpreter
+- you should use it by including it in your python software. See testmap.py for an example.
+Hint: add this line to use the library - 'from ifmap import ifmapClient' """
--- testmap.py
+++ testmap.py
@@ -1,6 +1,4 @@
-
-
-from ifmap import ifmapClient
+from ifmap import ifmapClient, IPAddress, ifmapIDFactory, MACAddress, Device, AccessRequest, Identity
 
 
 client = ifmapClient("https://127.0.0.1:8443", 'test', 'test')
@@ -8,3 +6,41 @@ client = ifmapClient("https://127.0.0.1:8443", 'test', 'test')
 client.connect()
 
 client.publishtest()
+
+print IPAddress("10.0.0.1","IPv4","ifmaplab")
+print IPAddress("10.0.0.1","IPv4",)
+print IPAddress("10.0.0.1",)
+print IPAddress("3ffe:1900:4545:3:200:f8ff:fe21:67cf","IPv6","ifmaplab")
+print IPAddress("3ffe:1900:4545:3:200:f8ff:fe21:67cf","IPv6",)
+print IPAddress("3ffe:1900:4545:3:200:f8ff:fe21:67cf",)
+print MACAddress("aa:bb:cc:dd:ee:ff","ifmaplab",)
+print MACAddress("aa:bb:cc:dd:ee:ff",)
+print Device("123:45")
+print Device("123:45","aikdummynamef34feccc28b3d44f")
+print AccessRequest("111:23","ifmaplab",)
+print AccessRequest("111:23",)
+print Identity("john.doe")
+print Identity("john.doe@example.com", type="email_address")
+print Identity("ef9b13e5df7dae502c51db7ca4624552", type="other", other_type="RFID")
+print Identity("ef9b13e5df7dae502c51db7ca4624552", type="other", other_type="RFID", administrative_domain="ifmaplab")
+
+
+"""
+Test Results
+<ip-address value="10.0.0.1" type="IPv4" administrative-domain="ifmaplab" />
+<ip-address value="10.0.0.1" type="IPv4" />
+<ip-address value="10.0.0.1" />
+<ip-address value="3ffe:1900:4545:3:200:f8ff:fe21:67cf" type="IPv6" administrative-domain="ifmaplab" />
+<ip-address value="3ffe:1900:4545:3:200:f8ff:fe21:67cf" type="IPv6" />
+<ip-address value="3ffe:1900:4545:3:200:f8ff:fe21:67cf" />
+<mac-address value="aa:bb:cc:dd:ee:ff" administrative-domain="ifmaplab" />
+<mac-address value="aa:bb:cc:dd:ee:ff" />
+<device><name>123:45</name></device>
+<device><name>123:45</name><aik-name>aikdummynamef34feccc28b3d44f<aik-name></device>
+<access-request name="111:23" administrative-domain="ifmaplab" />
+<access-request name="111:23" />
+<identity name="john.doe" />
+<identity name="john.doe@example.com" type="email_address" />
+<identity name="ef9b13e5df7dae502c51db7ca4624552" type="other" other-type="RFID" />
+<identity name="ef9b13e5df7dae502c51db7ca4624552" type="other" other-type="RFID" administrative-domain="ifmaplab" />
+"""
